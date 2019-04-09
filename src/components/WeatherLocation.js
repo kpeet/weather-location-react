804345d691e483579963b24c6f70b179aa751ca9
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-04-08 21:53:53 -0400

    First Step React Weather App

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,8 @@
+import React from 'react'; //importacion de react en el archivo
+
+const WeatherLocation = () => ( //codigo con react, Componente funcional
+    <div>Weather Location</div>
+);
+
+
+export default WeatherLocation;
