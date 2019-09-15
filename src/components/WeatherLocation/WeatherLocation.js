3d065c43ee467df258cf3371f9e33a9f9a180381
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-09-15 20:04:51 -0300

    fetch with promise to local account engine

--- src/components/WeatherLocation/WeatherLocation.js
+++ src/components/WeatherLocation/WeatherLocation.js
@@ -4,6 +4,12 @@ import WeatherData from './WeatherData/index'
 import  './styles.css';
 import {  SUN,WINDY } from "../../constants/weather";
 
+const location = "Buenos Aires, ar"
+const api_key = "f445f01ba4a7ed0327e5e7afd457b76d"
+const url_base_weather = "https://samples.openweathermap.org/data/2.5/weather"
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+const account_engine_url ="http://localhost:8007/accounts/"
 const data={
     temperature: 5,
     weatherState: SUN,
@@ -29,6 +35,14 @@ class WeatherLocation extends Component{
     }
 
     handleUpdateClick = () =>{
+        fetch(account_engine_url).then(resolve =>{
+
+            return resolve.json();
+        }).then(data =>{
+            console.log(data);
+            debugger;
+        });
+
         console.log("Actualizado");
         this.setState ( {
             city: "Buenos WENA",
