b098d0a8948c58d95854f7731f8ba6d04674f399
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-12-08 11:40:45 -0300

    nuevo parametro en Location

--- src/components/WeatherLocation2/WeatherLocation.js
+++ src/components/WeatherLocation2/WeatherLocation.js
@@ -5,7 +5,7 @@ import WeatherData from './WeatherData';
 const WeatherLocation = () =>  (
     <div>
         Weather Location
-        <Location/>
+        <Location city={"Buenos Aires"}/>
         <WeatherData/>
     </div>
 )
