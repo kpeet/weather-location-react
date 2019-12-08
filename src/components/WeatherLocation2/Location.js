e6ffc31aea8cbae758aa921cc78146e838bd623a
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-12-08 11:51:11 -0300

    first Destructuring for param city on Location for WeatherLocation

--- src/components/WeatherLocation2/Location.js
+++ src/components/WeatherLocation2/Location.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 
 const Location = (props) => {
-    const city = props.city
+    //Destructuring
+
+    const { city } = props;
+
+
+    //Sin Destructuring
+    const city2 = props.city
     console.log(props)
     //debugger;
 
     return (<div><h1>{city} <br/> Buenos Aires</h1></div>)
-}
+};
 
 
 
