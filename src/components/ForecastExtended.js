753a5d5ee5aa47049b2542f2d16e8ea889ced9d1
Kenneth Peet <kpeet@inf.utfsm.cl>
2019-12-15 17:21:22 -0300

    Iniciando cargador en forecastExtended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -19,19 +19,33 @@ const data = {
 
 class ForecastExtended extends Component {
 
+    constructor(){
+        super();
+        this.state = { forecatsData: null }
+    }
+
     renderForecatItemDay(){
         return days.map( day =>  ( <ForecastItem weekDay = {day} hour={10} data={data} ></ForecastItem>));
 
+    }
+    renderProgress = () => {
+        return "<h3>Cargando pronóstico extendido ...</h3>"
     }
 
 
     render(){
         // es casi lo mismo a const { city } = this.props; aqui estoy haciendo Destructuring
         const city = this.props.city;
+        const forecatsData = this.props.forecatsData;
 
         return (<div className={'forecastTitle'}>
             <h2  >Pronóstico Extendido para { city } </h2>
-            { this.renderForecatItemDay()}
+            { forecatsData ?
+
+                this.renderForecatItemDay():
+                this.renderProgress()
+            }
+
 
             </div>);
     }
